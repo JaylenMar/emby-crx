f8d7b31d506d38368816a155c7d34af95d279d2a
Jaylen.Mar <66826612+JaylenMar@users.noreply.github.com>
2025-02-21 23:55:14 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,10 +4,10 @@
 rm -rf emby-crx
 mkdir -p emby-crx
 wget https://raw.githubusercontent.com/JaylenMar/emby-crx/master/static/css/style.css -P emby-crx/
-wget https://raw.githubusercontent.com/amilys/emby-crx/master/static/js/common-utils.js -P emby-crx/
-wget https://raw.githubusercontent.com/amilys/emby-crx/master/static/js/jquery-3.6.0.min.js -P emby-crx/
-wget https://raw.githubusercontent.com/amilys/emby-crx/master/static/js/md5.min.js -P emby-crx/
-wget https://raw.githubusercontent.com/amilys/emby-crx/master/content/main.js -P emby-crx/
+wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/common-utils.js -P emby-crx/
+wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/jquery-3.6.0.min.js -P emby-crx/
+wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/md5.min.js -P emby-crx/
+wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/content/main.js -P emby-crx/
 
 # 读取index.html文件内容
 content=$(cat index.html)
